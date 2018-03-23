bd79ddf1683f9535e7682f2ecdc0bcbbef8cce9e
Aanpasser <34793861+Aanpasser@users.noreply.github.com>
2018-03-23 20:13:56 +0100

    Add files via upload

---
+++ Test-led-12-uit.ino
@@ -0,0 +1,36 @@
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+// Hier wordt het lcd scherm ingesteld
+int led[] = {2, 3, 4, 5, 6, 7, 8, 9};
+int wachtlus (750);
+int teller;
+int plaats;
+LiquidCrystal_I2C lcd(0x38, 16, 2); // Zet het i2c adres op 0x38 16 karakters en 2 regels.
+
+//
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd
+  lcd.backlight();
+  lcd.setCursor(2, 0);      //Zet de cursor op posite 4  regel 0
+  lcd.print("Jan Middel");
+
+
+  // put your setup code here, to run once:
+  for (teller = 2; teller <= 9; teller++)
+  pinMode(led[teller],OUTPUT);}
+
+
+void loop()
+{
+
+  //
+  for (plaats = 0 ; plaats < 8; plaats++);
+  delay (wachtlus);
+  digitalWrite(led[plaats], HIGH);
+  //lcd.setCursor(1, 1);
+  //lcd.print(plaats);
+
+}
